840e4ebc19e1a3cc301bf1e5c574cd988bc050f5
Joung Daun <i_co1022@naver.com>
2015-07-04 15:31:05 +0900

    Add comment
    add comment for test

--- app/src/main/java/com/example/ico/njnd_app/MainActivity.java
+++ app/src/main/java/com/example/ico/njnd_app/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Test start~!
         setContentView(R.layout.activity_main);
     }
 
