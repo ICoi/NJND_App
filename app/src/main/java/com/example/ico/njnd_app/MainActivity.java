9ab3522661e135f06012ca39b38701297700291b
JSungMin <jsm1505104@gmail.com>
2015-08-01 15:22:50 +0900

    Error in making button
    1

--- app/src/main/java/com/example/ico/njnd_app/MainActivity.java
+++ app/src/main/java/com/example/ico/njnd_app/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.ico.njnd_app;
 
+import android.os.Message;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.ImageView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.apache.http.Header;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-
         setOnClickListener();
+        img = (ImageView)findViewById(R.id.image);
     }
-
-
 
     public void setOnClickListener(){
         Button btn = (Button)findViewById(R.id.btn_activity_main_goNext);
--- app/src/main/java/com/example/ico/njnd_app/MenuListActivity.java
+++ app/src/main/java/com/example/ico/njnd_app/MenuListActivity.java
@@ -7,14 +7,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TableRow;
 
 
 public class MenuListActivity extends ActionBarActivity {
 
+    public int maxButtonNum = 12;
+    private Button content[];
+    private String text[] = {"123","345","456"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        content = new Button[12];
+
+        GridLayout grid = new GridLayout(this);
+
+        for(int num = 0;num<12; num++) {
+            TableRow row = new TableRow(this);
+                content[num] = new Button(this);
+                content[num].setText(text[num % 3]);
+                content[num].setId(num);
+                content[num].setTextSize(10f);
+            row.addView(content[num], 30, 30);
+            grid.addView(row);
+        }
         setContentView(R.layout.activity_menu_list);
+
+    /*
+        String text[] =  {"123","456","789"};
+
+        for (int i = 0 ; i < text.length; i++) {
+              this.text[i] = text[i];
+        }
+    */
+
+
+
         setOnClickListener();
     }
 
@@ -24,8 +54,20 @@ public class MenuListActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_menu_list, menu);
         return true;
     }
+
     public void setOnClickListener()
     {
+        for(int i = 0 ; i < 12 ; i++) {
+            final String name = text[i%3];
+            content[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent(MenuListActivity.this, ContentListActivity.class);
+                    in.putExtra("clickedContentName", name);
+                }
+            });
+        }
+
         Button btn = (Button)findViewById(R.id.btn_menu_list_goContentList);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +83,7 @@ public class MenuListActivity extends ActionBarActivity {
                 Intent i = new Intent(MenuListActivity.this,ClothListActivity.class);
                 startActivity(i);
             }
+
         });
     }
     @Override
