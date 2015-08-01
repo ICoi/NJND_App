52a5657a30a90573c7f7a40f212d078ccf04a229
Joung Daun <i_co1022@naver.com>
2015-08-01 17:28:24 +0900

    Cloth Page  화면 배치 구성 및 버튼 작동 과정 확인

--- app/src/main/java/com/example/ico/njnd_app/ClothActivity.java
+++ app/src/main/java/com/example/ico/njnd_app/ClothActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 
 public class ClothActivity extends ActionBarActivity {
@@ -12,9 +16,49 @@ public class ClothActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloth);
-        
+
+        setOnClickListener();
     }
-    
+
+    private void setOnClickListener(){
+        ImageButton headImgBtn = (ImageButton)findViewById(R.id.imgbtn_cloth_head);
+        headImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "head Button", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        ImageButton shirtsImgBtn = (ImageButton)findViewById(R.id.imgbtn_cloth_shirts);
+        shirtsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "shirts Button", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        ImageButton coatImgBtn = (ImageButton)findViewById(R.id.imgbtn_cloth_coat);
+        coatImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "coat Button", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        ImageButton pantsImgBtn = (ImageButton)findViewById(R.id.imgbtn_cloth_pants);
+        pantsImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "pants Button", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
