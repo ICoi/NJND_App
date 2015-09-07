ecf9995aa11461fc460c209a815d85091fe42ee8
Joung Daun <i_co1022@naver.com>
2015-09-07 17:05:01 +0900

    Login 부분 구현중 (쿠키 구현 완려!)

--- app/src/main/java/com/example/ico/njnd_app/MainActivity.java
+++ app/src/main/java/com/example/ico/njnd_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ico.njnd_app;
 
 import android.app.Activity;
+import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBarActivity;
@@ -10,16 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
 
 import org.apache.http.Header;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.cookie.BasicClientCookie;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -30,21 +36,43 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        setOnClickListener();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        PersistentCookieStore myCookieStore = new PersistentCookieStore(getApplicationContext());
+        List<Cookie> cookies = myCookieStore.getCookies();
+
+        if(cookies.isEmpty()){
+            Toast.makeText(getApplicationContext(), "no cookies", Toast.LENGTH_SHORT).show();
+
+            client.setCookieStore(myCookieStore);
+            BasicClientCookie newCookie = new BasicClientCookie("AppID", "awesome");
+            newCookie.setVersion(1);
+            newCookie.setDomain("namjungnaedle123.cafe24.com:3000");
+            newCookie.setPath("/");
+            myCookieStore.addCookie(newCookie);
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // Do something after 3s = 3000ms
+                    Intent i = new Intent(MainActivity.this, MenuListActivity.class);
+                    startActivity(i);
+                }
+            }, 3000);
+        }else{
+            Toast.makeText(getApplicationContext(),"HAS cookies // " + cookies.get(0).getName() + " - " + cookies.get(0).getValue(),Toast.LENGTH_SHORT).show();
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // Do something after 3s = 3000ms
+                    Intent i = new Intent(MainActivity.this, MenuListActivity.class);
+                    startActivity(i);
+                }
+            }, 3000);
+        }
         img = (ImageView)findViewById(R.id.image);
     }
-
-    public void setOnClickListener(){
-        Button btn = (Button)findViewById(R.id.btn_activity_main_goNext);
-
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(MainActivity.this, MenuListActivity.class);
-                startActivity(i);
-            }
-        });
-
-    }
-
 }
