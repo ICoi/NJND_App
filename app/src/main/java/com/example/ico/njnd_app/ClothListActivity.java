a7dd8933895140af0bf0d00606b56a8c24e61ef4
Joung Daun <i_co1022@naver.com>
2015-09-07 16:26:03 +0900

    추천 부분 웹뷰 적용

--- app/src/main/java/com/example/ico/njnd_app/ClothListActivity.java
+++ app/src/main/java/com/example/ico/njnd_app/ClothListActivity.java
@@ -7,50 +7,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 
 public class ClothListActivity extends Activity {
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloth_list);
+        webview = (WebView)findViewById(R.id.webview);
 
-
-        setOnClickListener();
+        webview.setWebViewClient(new WebClient()); // 응룡프로그램에서 직접 url 처리
+        WebSettings set = webview.getSettings();
+        set.setJavaScriptEnabled(true);
+        set.setBuiltInZoomControls(true);
+        webview.loadUrl("http://namjungnaedle123.cafe24.com:3000/web/recommend_menu");
     }
 
-    void setOnClickListener(){
-        Button btn = (Button)findViewById(R.id.btn_cloth_list_goNext);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ClothListActivity.this, ClothActivity.class);
-                startActivity(i);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_cloth_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    class WebClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
             return true;
+            }
         }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
