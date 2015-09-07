6ec198a4316764558ab911f7d2bf587514ad5168
Joung Daun <i_co1022@naver.com>
2015-09-07 16:39:44 +0900

    hot fashio과 cloth list 화면 분리

--- app/src/main/java/com/example/ico/njnd_app/HotFashionActivity.java
+++ app/src/main/java/com/example/ico/njnd_app/HotFashionActivity.java
@@ -1,39 +1,42 @@
 package com.example.ico.njnd_app;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
-public class ClothListActivity extends Activity {
+
+public class HotFashionActivity extends Activity {
     private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cloth_list);
+        setContentView(R.layout.activity_hot_fashion);
         webview = (WebView)findViewById(R.id.webview);
 
         webview.setWebViewClient(new WebClient()); // 응용프로그램에서 직접 url 처리
         WebSettings set = webview.getSettings();
         set.setJavaScriptEnabled(true);
         set.setBuiltInZoomControls(true);
-        webview.loadUrl("http://namjungnaedle123.cafe24.com:3000/web/fitting_room_list");
+        webview.loadUrl("http://namjungnaedle123.cafe24.com:3000/web/hot_fashion_list");
+
+
+
     }
 
     class WebClient extends WebViewClient {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
-            }
         }
-
+    }
 }
